ab9e768e00fd7ed43c9c133e145b0f9acb73cc61
Richard <rick_pendlebury@hotmail.com>
2016-01-11 15:05:51 +0000

    Add base functionality for stack widget implementation

--- app/src/main/java/barqsoft/footballscores/widget/FootballWidgetProvider.java
+++ app/src/main/java/barqsoft/footballscores/widget/FootballWidgetProvider.java
@@ -1,9 +1,13 @@
 package barqsoft.footballscores.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import barqsoft.footballscores.R;
 
@@ -12,24 +16,77 @@ import barqsoft.footballscores.R;
  */
 public class FootballWidgetProvider extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+    // These constants apply to the stack widget
+    public static final String TOAST_ACTION = "barqsoft.footballscores.TOAST_ACTION";
+    public static final String EXTRA_ITEM = "barqsoft.footballscores.EXTRA_ITEM";
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
+//    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+//                                int appWidgetId) {
+//
+//        CharSequence widgetText = context.getString(R.string.appwidget_text);
+//        // Construct the RemoteViews object
+//        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget);
+//        views.setTextViewText(R.id.appwidget_text, widgetText);
+//
+//        // Instruct the widget manager to update the widget
+//        appWidgetManager.updateAppWidget(appWidgetId, views);
+//    }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+//        for (int appWidgetId : appWidgetIds) {
+//            updateAppWidget(context, appWidgetManager, appWidgetId);
+//
+//        }
+        for (int i = 0; i < appWidgetIds.length; ++i) {
+
+            // Here we setup the intent which points to the StackViewService which will
+            // provide the views for this collection.
+            Intent intent = new Intent(context, FootballRemoteViews.class);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+            // When intents are compared, the extras are ignored, so we need to embed the extras
+            // into the data so that the extras will not be ignored.
+            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+
+            // Note these lines to refer to the StackWidget specific layouts
+            RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.stack_appwidget);
+            rv.setRemoteAdapter(appWidgetIds[i], R.id.stack_view, intent);
+
+            // The empty view is displayed when the collection has no items. It should be a sibling
+            // of the collection view.
+            rv.setEmptyView(R.id.stack_view, R.id.empty_view);
+
+            // Here we setup the a pending intent template. Individuals items of a collection
+            // cannot setup their own pending intents, instead, the collection as a whole can
+            // setup a pending intent template, and the individual items can set a fillInIntent
+            // to create unique before on an item to item basis.
+            Intent toastIntent = new Intent(context, FootballRemoteViews.class);
+            toastIntent.setAction(FootballWidgetProvider.TOAST_ACTION);
+            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+            PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+            rv.setPendingIntentTemplate(R.id.stack_view, toastPendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetIds[i], rv);
         }
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        AppWidgetManager mgr = AppWidgetManager.getInstance(context);
+        if (intent.getAction().equals(TOAST_ACTION)) {
+            int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+            int viewIndex = intent.getIntExtra(EXTRA_ITEM, 0);
+            Toast.makeText(context, "Touched view " + viewIndex, Toast.LENGTH_SHORT).show();
+        }
+        super.onReceive(context, intent);
+
     }
 
     @Override
---
+++ app/src/main/java/barqsoft/footballscores/widget/WidgetItem.java
@@ -0,0 +1,13 @@
+package barqsoft.footballscores.widget;
+
+/**
+ * Created by R.Pendlebury on 11/01/2016.
+ */
+public class WidgetItem {
+
+    public String text;
+
+    public WidgetItem(String text) {
+        this.text = text;
+    }
+}
